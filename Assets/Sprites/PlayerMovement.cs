507166fa9ea0ec8f39577a35c9373e97e2ea4604
Zachary Siapno <EWSB52@gmail.com>
2025-05-14 12:51:01 -0400

    Added all the Imports and Animations to the project

---
+++ Assets/Sprites/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Animator animator;
+    public float defaultHealth;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
