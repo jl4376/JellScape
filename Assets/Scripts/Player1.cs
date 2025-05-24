e3a15210d20bdc53bdffb9c11ccce563bd5dfada
Jeffrey Luong <jl4376@drexel.edu>
2025-05-24 14:47:02 -0400

    added health bar and fixed player and hazard logic

--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -1,101 +1,19 @@
 using UnityEngine;
-using System.Collections;
 
 public class Player1 : Player
 {
-    //public Animator animator;
-    private int defaultHealth;
-    public static Player1 Instance;
-    private Vector3 initalLocation;
-    public static bool isDestroyed = false;
-    // [SerializeField] private GameObject playerPrefab;
-
-    private bool canDash = true;
-    private bool isDashing;
-    public float dashingPower;
-    public float dashingTime;
-    public float dashingCooldown;
-
-    [SerializeField] private TrailRenderer tr;
-
-    void Awake() {
-        if (Instance == null) {
-            Instance = this;
-        }
+    protected override Vector2 ReadMovementInput()
+    {
+        var dir = Vector2.zero;
+        if      (Input.GetKey(KeyCode.W)) dir.y = +1;
+        else if (Input.GetKey(KeyCode.S)) dir.y = -1;
+        if      (Input.GetKey(KeyCode.A)) dir.x = -1;
+        else if (Input.GetKey(KeyCode.D)) dir.x = +1;
+        return dir;
     }
 
-    protected override void CustomStart() {
-        initalLocation = rigidBody.transform.position;
-        defaultHealth = health;
+    protected override bool ReadDashInput()
+    {
+        return Input.GetKeyDown(KeyCode.Tab);
     }
-
-    protected override void Move() {
-    if (isDashing) {
-        return;
-    }
-
-    if (moveDirection.magnitude > 0) {
-        rigidBody.linearVelocity = moveDirection * moveSpeed;
-    } else {
-        // Slowly reduce velocity and clamp it to zero when small
-        rigidBody.linearVelocity *= (1f - friction);
-
-        if (rigidBody.linearVelocity.magnitude < 0.01f) {
-            rigidBody.linearVelocity = Vector2.zero;
-        }
-    }
-}
-
-    void Update() {
-        moveDirection = Vector2.zero;
-
-        // Collect input
-        if (Input.GetKey(KeyCode.W)) {
-            moveDirection.y = 1;
-        } else if (Input.GetKey(KeyCode.S)) {
-            moveDirection.y = -1;
-        }
-
-        if (Input.GetKey(KeyCode.A)) {
-            moveDirection.x = -1;
-        } else if (Input.GetKey(KeyCode.D)) {
-            moveDirection.x = 1;
-        }
-
-        // Set walking animation based on movement vector
-        //animator.SetBool("isWalk", moveDirection != Vector2.zero);
-
-        if (Input.GetKeyDown(KeyCode.Tab) && canDash) {
-            StartCoroutine(Dash());
-        }
-
-    }
-
-    private IEnumerator Dash() {
-        canDash = false;
-        isDashing = true;
-
-        Vector2 dashDir = moveDirection.magnitude > 0 ? moveDirection.normalized : Vector2.right * transform.localScale.x;
-        rigidBody.linearVelocity = dashDir * dashingPower;
-
-        tr.emitting = true;
-        yield return new WaitForSeconds(dashingTime);
-        tr.emitting = false;
-        isDashing = false;
-        yield return new WaitForSeconds(dashingCooldown);
-        canDash = true;
-    }
-
-    public void TakeDamage(int amount) {
-        health -= amount;
-       //animator.setBool();
-        if (health <= 0)
-        {
-            Debug.Log("Player has died!");
-            gameObject.SetActive(false); // Or trigger death animation, etc.
-        } else {
-            Debug.Log($"Player has {health} health left");
-        }
-    }
-
 }
